84b4e77ff4f4fa4187f14a68eb2dcb9b9d64f03e
rocka <i@rocka.me>
2017-12-08 13:53:12 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    // title of all HTML pages. Cannot be null.
+    title: 'NeoBlog',
+    // local server port. default to `2233` .
+    port: 2233,
+    // path to article directory. default to `./article` .
+    articleDir: './article',
+    // path to template directory. set `null` to use built-in template.
+    templateDir: './node_modules/neoblog-template-teal/template',
+    // plugins to load. At least an empty array.
+    plugins: [
+    ],
+    // arguments passed to template. can be anything but null.
+    templateArgs: {
+        nav: [
+            {
+                name: 'Index',
+                link: '/'
+            }
+        ],
+        side: [
+            {
+                name: 'Info',
+                items: [
+                    `OS: ${process.platform} ${process.arch}`,
+                    `Node: ${process.version}`
+                ]
+            }
+        ]
+    }
+};
